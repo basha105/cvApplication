125dda4790271f14ead2ab2afd7ca13212ae8af4
basha105 <brentbasha17@gmail.com>
2024-11-25 15:57:53 -0800

    Finished

--- src/components/workxp.jsx
+++ src/components/workxp.jsx
@@ -2,20 +2,49 @@ import { useState } from 'react';
 
 export default function WorkExperience() {
     const [submitted, changeSubmitted] = useState(false);
-    
+    const [jobTitle, changeJobTitle] = useState('');
+    const [companyName, changeCompanyName] = useState('');
+    const [jobDesc, changejobDesc] = useState('');    
+
     function handleSubmit() {
         changeSubmitted(!submitted);
     }
 
     return (
         <>
+        <h2>Work Experience</h2>
         {submitted ? (
             <div>
-                <div>Insert info here</div>
-                <button onClick={handleSubmit}></button>
-
+                <div>{companyName}</div>
+                <div>{jobTitle}</div>
+                <div>{jobDesc}</div>
+                <button onClick={handleSubmit}>Edit</button>
             </div>
-        )}
+        ) :
+        <form onSubmit={(e) => {
+            e.preventDefault();
+            handleSubmit();
+        }}>
+
+            <div>
+                <label htmlFor="company">Company: </label>
+                <input type="text" placeholder="Company" value={companyName} onChange={(e) => changeCompanyName(e.target.value)}/>
+            </div>
+        
+            <div>
+                <label htmlFor="jobTitle">Job title: </label>
+                <input type="text" placeholder="Job title" value={jobTitle} onChange={(e) => changeJobTitle(e.target.value)}/>
+            </div>
+
+            <div>
+                <div>Job Description:</div>
+                <textarea placeholder="Describe your position..." value={jobDesc} onChange={(e) =>changejobDesc(e.target.value)} cols="30" rows="10"></textarea>
+            </div>
+
+           <button type="submit">Submit</button>
+
+        
+        </form>}
         </>
     )
 
--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,12 @@ import './index.css'
 import App from './App.jsx'
 import GeneralInfo from './components/genInfo.jsx'
 import Education from './components/educ.jsx'
+import WorkExperience from './components/workxp.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <GeneralInfo />
     <Education />
+    <WorkExperience />
   </StrictMode>,
 )
